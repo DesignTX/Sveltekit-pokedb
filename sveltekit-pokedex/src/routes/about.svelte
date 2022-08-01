73c7c94c969bda05c686c2241b974479e9ea402e
Tony Xu <Design.TXU@gmail.com>
2022-08-02 09:39:53 +1000

    Initial Commit

---
+++ sveltekit-pokedex/src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>Svelte Kit demo that uses Pokemon API to build a pokedex</p>
---
+++ sveltekit-pokedex/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    console.log("hello world")
+</script>
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+<h1>Welcome to SvelteKit - Updated</h1>
+
