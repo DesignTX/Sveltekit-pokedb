a409e0fbf260e60c2e30c93ba352c1f7fe8d2939
Tony Xu <Design.TXU@gmail.com>
2022-08-02 10:54:09 +1000

    Created pokemon search function

--- sveltekit-pokedex/src/routes/index.svelte
+++ sveltekit-pokedex/src/routes/index.svelte
@@ -1,6 +1,17 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
 	import PokemanCard from '../components/pokemanCard.svelte';
+
+    let searchTerm = "";
+    let filteredPokemon = [];
+    $: {
+        console.log(searchTerm);
+        if(searchTerm){
+            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.includes(searchTerm.toLowerCase()));
+        }else {
+            filteredPokemon = [...$pokemon]
+        }
+    }
 </script>
 
 <svelte:head>
@@ -8,6 +19,10 @@
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-{#each $pokemon as pokeman}
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
+
+<div class=" py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+{#each filteredPokemon as pokeman}
 	<PokemanCard pokeman={pokeman}/>
 {/each}
+</div>
